b41c24b6e5232f47ef1988cce961291166be9dcc
aryan singh <aryan@payzli.com>
2025-08-17 16:25:54 +0530

    adding binary heap

---
+++ BinaryHeap.cs
@@ -0,0 +1,16 @@
+using System;
+public class Node
+{
+    public int Key;
+    public Node Left, Right;
+
+    public Node(int item)
+    {
+        Key = item;
+        Left = Right = null;
+    }
+}
+class BinaryHeap
+{
+
+}
