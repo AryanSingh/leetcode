2fd3cb23089249a53922db547daa8c206bec6ab6
aryan singh <aryan@payzli.com>
2025-08-21 14:33:14 +0530

    stashing

--- MinBinaryHeap.cs
+++ MinBinaryHeap.cs
@@ -1,19 +1,8 @@
 using System;
-public class Node
-{
-    public int Key;
-    public Node Left, Right;
-
-    public Node(int item)
-    {
-        Key = item;
-        Left = Right = null;
-    }
-}
 class MinBinaryHeap
 {
     public List<int> _heap;
-    public BinaryHeap()
+    public MinBinaryHeap()
     {
         _heap = new List<int>();
     }
@@ -27,32 +16,71 @@ class MinBinaryHeap
 
     public int GetMin()
     {
-        return Root.Key;
+        if (_heap.Count > 0)
+        {
+            return _heap[0];
+        }
+        throw new InvalidOperationException("Heap is empty");
+
     }
 
     public int Extract()
     {
+        if (_heap.Count > 0)
+        {
+            var value = _heap[0];
+            _heap[0] = _heap[_heap.Count - 1];
+            _heap.RemoveAt(_heap.Count - 1);
+            if(_heap.Count>0)HeapifyDown(0);
+            return value;
+        }
+        throw new InvalidOperationException("heap is empty");
 
     }
 
     private void HeapifyUp(int index)
     {
-        var parent = Math.Floor((index - 1) / 2);
-        if(h)
+        int parent = (index - 1) / 2;
+        if (parent >= 0 && _heap[parent] > _heap[index])
+        {
+            (_heap[parent], _heap[index]) = (_heap[index], _heap[parent]);
+            HeapifyUp(parent);
+        }
+        else return;
 
     }
 
 
     private void HeapifyDown(int index)
     {
+        var leftIndex = 2 * index + 1;
+        var rightIndex = 2 * index + 2;
+        int smallest = index;
+
+        if (leftIndex < _heap.Count && _heap[smallest] > _heap[leftIndex])
+        {
+            smallest = leftIndex;
+        }
+        if (rightIndex < _heap.Count && _heap[smallest] > _heap[rightIndex])
+        {
+            smallest = rightIndex;
+        }
+        if (smallest != index)
+        {
+            (_heap[smallest], _heap[index]) = (_heap[index], _heap[smallest]);
+            HeapifyDown(smallest);
+        }
 
     }
 
-    int Size() { }
+    int Size()
+    {
+        return _heap.Count;
+    }
 
     bool IsEmpty()
     {
-
+        return _heap.Count == 0;
     }
 
 
